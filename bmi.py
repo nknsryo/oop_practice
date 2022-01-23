0e33f596f8d6e06fc8f53792c86bbb2a339e3bbe
nknsryo <nkns.ryo@gmail.com>
2022-01-23 17:56:32 +0900

    最終地点でいらないものコメントアウト

--- bmi.py
+++ bmi.py
@@ -1,4 +1,29 @@
 """
+clss BMI:
+    関連する属性:
+        -身長(m)
+        -体重(kg)
+        -BMIという値そのもの
+    ルール:
+        -BMIは22が適正 10以上　40以下の範囲<=常識的な範囲
+        -表示する時には小数点以下第2位まで四捨五入
+            -ex:23.689 =>23.69
+            -ex:23.681 =>23.68
+    できること:
+        -BMIの計算
 
 
 """
+
+
+class BMI:
+    def __init__(self, height_m, weight_kg):
+        # self.height_m = height_m
+        # self.weight_kg = weight_kg
+        self.value = self.weight_kg / self.height_m ** 2
+
+
+noriya = BMI(height_m=1.7, weight_kg=65)
+# print(noriya.height_m)
+# print(noriya.weight_kg)
+print(noriya.value)

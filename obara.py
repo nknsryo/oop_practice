689cf9ae3217423bc536db085b14d820527aa947
nknsryo <nkns.ryo@gmail.com>
2022-01-23 18:22:29 +0900

    完成

--- obara.py
+++ obara.py
@@ -1,16 +1,12 @@
-
-import self as self
-
-
 class BMI:
     def __init__(self, height_m, weight_kg):
         self.value = weight_kg / height_m ** 2
         if not (10 <= self.value <= 40):
-            raise ValueError(f"MBIが正常値を外れています!!{self.value}")
+            raise ValueError(f"BMIが正常値を外れています!!!{self.value}")
 
     def __str__(self):
         return f"{self.value:.2f}"
 
 
-noriya = BMI(height_m=1.7, weight_kg=65)
-print(noriya.value)
+noriya = BMI(height_m=1.0, weight_kg=65)
+print(noriya)  # 22.49
